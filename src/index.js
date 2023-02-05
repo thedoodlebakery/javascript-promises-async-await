159756689338feca7e60ab1fd4d80372d0f1f69f
Paula C <bakerp24@gmail.com>
2023-02-05 16:26:15 -0600

    setting up index js file with fetchMovies and moviePromise

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-export function fetchMovies(){};
+import movies from "data/movies.json";
+import fetchWithTimeout from "./services";
+
+export function fetchMovies(){
+    resolveFunction = () => movies;
+    fetchWithTimeout(1000)
+    .then(
+        resolveFunction()
+    )
+    
+}
+
+const moviePromise = fetchMovies()
+    .then(
+        resolveFunction (results){
+            console.log(results)
+        }
+)
