d1565ed978bc3a56160557178b697942419f5559
Paula C <bakerp24@gmail.com>
2023-02-05 17:44:01 -0600

    Added async for booksORmovies and booksANDmovies.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {fetchWithTimeout, fetchMovies, fetchBooks} from './services';
+import {fetchWithTimeout, fetchMovies, fetchBooks, asyncFetchBooks, asyncFetchMovies} from './services';
 const movies = require ('./data/movies.json')
 
 // export function fetchMovies(){
@@ -36,3 +36,36 @@ function getBooksOrMovies(){
 const getBooksOrMoviesPromise = getBooksOrMovies();
 getBooksOrMoviesPromise.then((results) => {
     console.log('getBooksOrMoviesPromise', results)});
+
+async function getBooksAndMoviesAsync(){
+    try{
+        const [ books, movies] =
+        await Promise.all([asyncFetchBooks(), asyncFetchMovies()]);
+        return { books, movies };
+    }catch(error) {
+        console.log("Error fetching books and movies", error);
+    }
+}
+
+async function getBooksOrMoviesAsync(){
+    try{
+        const values =
+        await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+        return values;
+    }catch(error) {
+        console.log("Error waiting for the promise race", error);
+    }
+}
+
+getBooksAndMoviesAsync().then((results) => {
+    console.log("movies and books", {
+        movies: results.movies,
+        books: results.books
+      });
+});
+
+getBooksOrMoviesAsync().then((results) => {
+    console.log("movies OR books", {
+        results
+    });
+});
