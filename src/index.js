ad9e4f5587b26345e25d83ddda950400c9fa1dbf
Paula C <bakerp24@gmail.com>
2023-02-05 17:06:34 -0600

    Moved the fetch functions to services js and created functions to build the arrays for books and movies.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import {fetchWithTimeout} from './services';
+import {fetchWithTimeout, fetchMovies, fetchBooks} from './services';
 const movies = require ('./data/movies.json')
 
-export function fetchMovies(){
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+// export function fetchMovies(){
+//     const resolveFunction = () => movies;
+//     return fetchWithTimeout(1000).then(resolveFunction);
     
+// }
+
+// const moviePromise = fetchMovies();
+// moviePromise.then((results) => {
+//     console.log(results);
+//     }
+// )
+
+function getBooksAndMovies(){
+    return Promise.all(
+        [fetchBooks(), fetchMovies()]
+    ).then(([books, movies ]) => ({
+        books,
+        movies
+      }))
+      .catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
-moviePromise.then((results) => {
-    console.log(results);
-    }
-)
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then((results) => {
+    console.log('getBooksAndMoviesPromise', results)});
+
+function getBooksOrMovies(){
+    return Promise.race([fetchBooks(), fetchMovies()])
+    .then(results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then((results) => {
+    console.log('getBooksOrMoviesPromise', results)});
